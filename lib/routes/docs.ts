db775f7914e06f8bfbc3e92c7c82c430f9781c46
guckin <donald.stephen.mcguckin@gmail.com>
2019-03-06 20:18:47 -0800

    removed swagger routes

--- lib/routes/docs.ts
+++ lib/routes/docs.ts
@@ -3,16 +3,14 @@ import * as Koa from 'koa'; import * as path from 'path';
 import * as Router from 'koa-router';
 
 
-const swaggerFilePath = path.join(__dirname, '..', '..', '..', 'docs', 'swagger.json');
-const swaggerFile = require(swaggerFilePath);
-swaggerFile.info.version = config.version;
-
 export function attachTo(router: Router) {
     router
         .all('/', (ctx: Koa.Context) => {
-            ctx.body = swaggerFile;
+            ctx.body = {healthCheck: 'ok'};
+            ctx.status = 200;
         })
         .all('/version', (ctx: Koa.Context) => {
             ctx.body = config.version;
+            ctx.status = 200;
         });
 };
