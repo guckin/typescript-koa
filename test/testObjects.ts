79314e13a313c2651d2d8d1cdddf8e64a87a7392
guckin <donald.stephen.mcguckin@gmail.com>
2019-03-11 22:46:30 -0700

    added test object generator

---
+++ test/testObjects.ts
@@ -0,0 +1,5 @@
+const sinon = require('sinon');
+
+export function createMockInstance(klass: any, overrides: object): any{
+  return sinon.createStubInstance(klass, overrides);
+}
---
+++ test/unit/services/cat.service.test.ts
@@ -0,0 +1,63 @@
+import { postCat, getCat } from '../../../lib/providers/cat.provider'
+import { expect } from 'chai'
+
+const sinon = require('sinon');
+
+describe('cat.service', async () => {
+
+  let dataStore: any;
+  let ctx: any;
+
+  beforeEach(() => {
+    // TODO: Posibily use a stub class
+    dataStore = {};
+    
+    const createCatMapper =  {
+      put: sinon.stub(),
+      get: sinon.stub(),
+      
+    };
+  });
+
+
+  describe('createCat', async () => {
+    it('400 when name is not provided in request body', async () => {
+      ctx.request.body = {};
+      await postCat(ctx);
+      expect(ctx.body).to.have.property('error');
+      expect(ctx.status).to.equal(400);
+    });
+
+    it('creates a cat', async () => {
+      const cat = 'my cat';
+      dataStore['createCat'] = sinon.fake.returns(cat);
+      await postCat(ctx);
+      expect(ctx.body).to.equal(cat);
+      expect(ctx.status).to.equal(201);
+    });
+
+    it('500 when a failure occurs', async () => {
+      dataStore['createCat'] = () => {throw 42}
+      await postCat(ctx);
+      expect(ctx.body).to.have.property('error');
+      expect(ctx.status).to.equal(500);
+    });
+  });
+
+  describe('getCat', async () => {
+    it('gets a cat', async () => {
+      const cat = 'my cat'
+      dataStore['retrieveCat'] = sinon.fake.returns(cat);
+      await getCat(ctx);
+      expect(ctx.body).to.equal(cat)
+      expect(ctx.status).to.equal(200)
+    });
+
+    it('500 when a failure occurs', async () => {
+      dataStore['retrieveCat'] = () => {throw 42}
+      await postCat(ctx);
+      expect(ctx.body).to.have.property('error');
+      expect(ctx.status).to.equal(500);
+    });
+  });
+});
